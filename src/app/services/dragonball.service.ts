3c26bdf5794908c5b260f038cee5a575338e7efb
SHIM3Z <shim3z151002@gmail.com>
2025-03-18 10:34:56 -0500

    fin de la seccion 5

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,26 +1,21 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
-@Injectable({providedIn: 'root'})
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+};
+
+@Injectable({ providedIn: 'root' })
 export class DragonBallService {
-  characters = signal<Character[]>([
-      {
-        id: 1,
-        name: 'Goku',
-        power: 9001,
-      },
-      {
-        id: 2,
-        name: 'Vegeta',
-        power: 8000,
-      }
-    ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
 
-    saveToLocalStorage = effect(() => {
-      localStorage.setItem('characters', JSON.stringify(this.characters()));
-    })
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
-    addCharacter(character: Character) {
-      this.characters.update((list) => [...list, character]);
-    }
+  addCharacter(character: Character) {
+    this.characters.update((list) => [...list, character]);
+  }
 }
